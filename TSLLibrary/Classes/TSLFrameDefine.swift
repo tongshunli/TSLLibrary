7d4f79e1cc9a36d14b4c886bc1d5c1e1996c1b7a
ShunLi Tong <13466621196@163.com>
2021-12-01 10:04:55 +0800

    feat(all): 修改宏定义

--- TSLLibrary/Classes/TSLFrameDefine.swift
+++ TSLLibrary/Classes/TSLFrameDefine.swift
@@ -6,34 +6,32 @@
 //
 
 import UIKit
-
-public struct TSLFrameDefine {
     
-    public static let kScreenWidth: CGFloat = UIScreen.main.bounds.size.width
+public static let kScreenWidth: CGFloat = UIScreen.main.bounds.size.width
 
-    public static let kScreenHeight: CGFloat = UIScreen.main.bounds.size.height
+public static let kScreenHeight: CGFloat = UIScreen.main.bounds.size.height
 
-    public static let kMargin: CGFloat = 20.0
+public static let kMargin: CGFloat = 20.0
 
-    public static let kMoreHalfMargin: CGFloat = 15.0
+public static let kMoreHalfMargin: CGFloat = 15.0
 
-    public static let kHalfMargin: CGFloat = 10.0
+public static let kHalfMargin: CGFloat = 10.0
 
-    public static let kQuarterMargin: CGFloat = 5.0
+public static let kQuarterMargin: CGFloat = 5.0
 
-    public static let kLineHeight: CGFloat = 0.5
+public static let kLineHeight: CGFloat = 0.5
 
-    public static let kStatusBarHeight = UIApplication.shared.windows.first?.safeAreaInsets.top ?? 20
+public static let kStatusBarHeight = UIApplication.shared.windows.first?.safeAreaInsets.top ?? 20
 
-    public static let kNavbarHeight = kStatusBarHeight + 44
+public static let kNavbarHeight = kStatusBarHeight + 44
 
-    public static let kTabbarHeight = UIApplication.shared.windows.first?.safeAreaInsets.bottom ?? 0 + 49
+public static let kTabbarHeight = UIApplication.shared.windows.first?.safeAreaInsets.bottom ?? 0 + 49
 
-    public func kFont(_ fontSize: CGFloat) -> UIFont {
-        return UIFont.systemFont(ofSize: fontSize)
-    }
-
-    public func kBoldFont(_ fontSize: CGFloat) -> UIFont {
-        return UIFont.boldSystemFont(ofSize: fontSize)
-    }
+public func kFont(_ fontSize: CGFloat) -> UIFont {
+    return UIFont.systemFont(ofSize: fontSize)
 }
+
+public func kBoldFont(_ fontSize: CGFloat) -> UIFont {
+    return UIFont.boldSystemFont(ofSize: fontSize)
+}
+
--- TSLLibrary/Classes/TSLSystemConfig.swift
+++ TSLLibrary/Classes/TSLSystemConfig.swift
@@ -6,33 +6,29 @@
 //
 
 import UIKit
+  
+public static let kDocumentPath = NSSearchPathForDirectoriesInDomains(.documentDirectory, .userDomainMask, true)
 
-public struct TSLSystemConfig {
-    
-    public static let kDocumentPath = NSSearchPathForDirectoriesInDomains(.documentDirectory, .userDomainMask, true)
+public static let kCachePath = NSSearchPathForDirectoriesInDomains(.cachesDirectory, .userDomainMask, true)
 
-    public static let kCachePath = NSSearchPathForDirectoriesInDomains(.cachesDirectory, .userDomainMask, true)
-
-    public static var keyWindow: UIWindow? = {
-        var window: UIWindow?
-        if #available(iOS 13.0, *) {
-            for window in UIApplication.shared.windows {
-                if window.isHidden == false {
-                    return window
-                }
+public static var keyWindow: UIWindow? = {
+    var window: UIWindow?
+    if #available(iOS 13.0, *) {
+        for window in UIApplication.shared.windows {
+            if window.isHidden == false {
+                return window
             }
-        } else {
-            window = UIApplication.shared.keyWindow
         }
-        return window
-    }()
+    } else {
+        window = UIApplication.shared.keyWindow
+    }
+    return window
+}()
 
-    public static let kAppCurrentVersion = Bundle.main.object(forInfoDictionaryKey: "CFBundleShortVersionString") // 获取当前版本号
+public static let kAppCurrentVersion = Bundle.main.object(forInfoDictionaryKey: "CFBundleShortVersionString") // 获取当前版本号
 
-    public static let kSystemVersion = UIDevice.current.systemVersion // 获取设备系统号
+public static let kSystemVersion = UIDevice.current.systemVersion // 获取设备系统号
 
-    public static let kIsIphone = UIDevice.current.userInterfaceIdiom == UIUserInterfaceIdiom.phone ? true : false // iphone设备
+public static let kIsIphone = UIDevice.current.userInterfaceIdiom == UIUserInterfaceIdiom.phone ? true : false // iphone设备
 
-    public static let kIsIpad = UIDevice.current.userInterfaceIdiom == UIUserInterfaceIdiom.pad ? true : false // ipad设备
-
-}
+public static let kIsIpad = UIDevice.current.userInterfaceIdiom == UIUserInterfaceIdiom.pad ? true : false // ipad设备
